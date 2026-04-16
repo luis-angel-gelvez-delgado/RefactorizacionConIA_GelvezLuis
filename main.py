31a5880f697f3e366747469796f4fb01a3370e76
Luis <lusiangelgelvezdelgado@gmail.com>
2026-04-16 08:50:35 -0500

    feat: :fire: add backup

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+Punto de entrada principal del sistema "Tu Amigo del Vecindario".
+Gestión comunitaria de herramientas, usuarios y préstamos.
+"""
+
 from menu_general import menu_general
 
-menu_general()
+
+if __name__ == '__main__':
+    menu_general()
